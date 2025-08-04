e1fdcbcdc01ab3b87f7614bfead451a5aefd8f45
Priyansh Solanki <priyansh.s@latitudetechnolabs.org>
2025-08-04 14:02:59 +0530

    added home page

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def welcome():
     return "Hello, Flask!"
+
+@app.route("/home")
+def home():
+    return "Home Page"
 
 if __name__ == "__main__":
     app.run(debug=True)
